1e10908ba9ac92c9058ed83e813726bf0fc0b468
Nick Setliff <NickSet@users.noreply.github.com>
2016-11-10 20:58:36 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,13 +3,11 @@ import PackageDescription
 let package = Package(
     name: "Silica",
     targets: [
-
         Target(
             name: "SilicaTests",
             dependencies: [.Target(name: "Silica")]),
         Target(
             name: "Silica")
-
     ],
     dependencies: [
         .Package(url: "https://github.com/PureSwift/Cairo.git", majorVersion: 1)
